9cc4ce58f7a3b6243e4eb30f6a27adc9cfd313a4
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-12 00:27:02 -0500

    Array of the 5 weeks per book example

--- Expressions-Worksheet/AverageShoppingBill/js/script.js
+++ Expressions-Worksheet/AverageShoppingBill/js/script.js
@@ -2,3 +2,10 @@
 // Christina Thorpe
 // February 11, 2014
 
+//Array of the 5 weeks per book example:
+var myArray = new Array();
+myArray[0] = "week1";
+myArray[1] = "week2";
+myArray[2] = "week3";
+myArray[3] = "week4";
+myArray[4] = "week5";
