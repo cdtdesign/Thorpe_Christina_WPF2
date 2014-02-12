7a52b0dfcc790bfca02c0970637f5d646df9dca5
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-12 01:35:06 -0500

    Edits to comments

--- Expressions-Worksheet/AverageShoppingBill/js/script.js
+++ Expressions-Worksheet/AverageShoppingBill/js/script.js
@@ -5,7 +5,7 @@
 // Array of the 5 weeks per class example:
 var week = [20.00, 25.00, 18.00, 22.00, 28.00];
 
-// Total spending of the 5 weeks:
+// Total spending of the 5 weeks + average:
 var total = week[0] + week[1] + week[2] + week[3] + week[4];
 weeklyAvg = total / 5;
 
