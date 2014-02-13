cccb0cd1f821e2e6dbe5dfa1961612d7468d5966
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-13 17:09:13 -0500

    Prompts and Alerts: javascript

---
+++ Prompts & Alerts/js/script.js
@@ -0,0 +1,17 @@
+// Prompt and Alert
+
+/* var yearBorn = prompt("Enter your year of birth");
+console.log(yearBorn); */
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width:");
+var height = prompt("Now please enter the height:");
+var area = width * height;
+
+//console.log(area); // prints it out to the console
+
+//alert("The area of your rectangle is " + area + " sq. feet");
+
+//the alert just above works the same as the alert below-both are correct:
+var result = "The area of your rectangle is " + area + " sq. feet";
+alert(result);
+
