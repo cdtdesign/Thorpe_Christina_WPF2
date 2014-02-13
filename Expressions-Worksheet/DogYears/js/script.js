30b506592800624ddd39728fe005f1ca7244afaf
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-13 06:50:06 -0500

    Dog Years script

--- Expressions-Worksheet/DogYears/js/script.js
+++ Expressions-Worksheet/DogYears/js/script.js
@@ -2,3 +2,12 @@
 // Christina Thorpe
 // February 11, 2014
 
+// List variables
+var humanAge = 5;
+var dogAge = humanAge * 7;
+
+// Calculate Sparky's age in dog years
+var result = "Sparky is " + humanAge + " human years old, which is " + dogAge + " in dog years.";
+
+// Print resulting statement
+console.log(result)
