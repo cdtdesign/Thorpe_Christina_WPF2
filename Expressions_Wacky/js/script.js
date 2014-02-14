1b8a017feb207021cd0683e85c3be25c2d965cf5
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-14 02:51:55 -0500

    Assignment op calculus the Martian craters

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -11,7 +11,7 @@
 
 // Create alert that tells the number of total Martian population
 var martianPop = alert("The total number of Martians on the planet Mars is 2,200.");
-// Variable representing total Martian populaion on Mars
+// Variable representing total Martian population on Mars
 var totalMartians = 2200;
 
 
@@ -22,12 +22,21 @@ var greenMartians = prompt("How many green Martians do you count on the planet? 
 
 
 // Create parse variable
-var martianInt;
+var martianInt1;
 // Create parse integer that subtracts the number of purple and green Martians from the total Martian population to reveal the number of orange Martians on the planet
-martianInt = parseInt(totalMartians - purpleMartians - greenMartians);
-var martianCount = alert("According to your count, there are " + martianInt + " orange Martians on the planet.");
+martianInt1 = parseInt(totalMartians - purpleMartians - greenMartians);
+var martianCount = alert("According to your count, there are " + martianInt1 + " orange Martians on the planet.");
 
 
-// Concatenate string for result statement
-var martianString = "The number of purple inhabitants living on Mars is " + purpleMartians + ". The number of green inhabitants is " + greenMartians + " and the number of orange inhabitants is " + martianInt + ". " + "The total number of inhabitants on the red planet is " + totalMartians + ". Fortunately, Martians are color blind, so they are all one and the same to one another!";
+// Concatenate first string for resulting statement
+var martianString1 = "The number of purple inhabitants living on Mars is " + purpleMartians + ". The number of green inhabitants is " + greenMartians + " and the number of orange inhabitants is " + martianInt1 + ". " + "The total number of inhabitants on the red planet is " + totalMartians + ". Fortunately, Martians are color blind, so they are all one and the same to one another!";
 
+
+
+// Assignment operator calculates the Martian craters
+var martianCrater = 30;
+martianCrater -= 10;
+
+
+// Print result in console
+console.log(martianString1 + " The purple Martians live in " + martianCrater + " of the 30 craters on the red planet.");
