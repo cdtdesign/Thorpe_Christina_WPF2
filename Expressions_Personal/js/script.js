839ca5d9930298cc1d1cc4fbd78c61afb2575113
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-13 20:09:04 -0500

    Assign variables and prompts

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,8 @@
-// Expressions_Personal
+// Expressions_Personal - Christian's School Night Schedule
 // Christina Thorpe
 // February 13, 2014
 
+// Assign variables and prompts
+var readingHours = prompt("Enter number of hours Christian reads on school nights:", 1);
+var gamingHours = prompt("Enter number of hours Christian games on school nights:", 1);
+var homewrkHours = prompt("Enter number of hours Christian does homework on school nights:", 1.5);
