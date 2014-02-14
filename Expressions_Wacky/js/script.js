25743cd608605dea1857d0809e164584a46e9c8d
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-14 00:00:28 -0500

    Enter variables of Martian population

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,4 +1,9 @@
-// Expressions_Wacky
+// Expressions_Wacky - There IS Life on Mars!!!
 // Christina Thorpe
 // February 13, 2014
 
+
+// Enter variables of Martian population
+var purpleMartians = 400
+var orangeMartians = 800
+var greenMartians = 1000
