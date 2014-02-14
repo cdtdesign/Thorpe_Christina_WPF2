b99e94370cce9456f91ad5c8b1a133bb8243da10
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-13 20:33:46 -0500

    Edits to variables and prompts; Calculate 4 week totals of Christian's activities

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,8 +1,13 @@
-// Expressions_Personal - Christian's School Night Schedule
+// Expressions_Personal - Christian's School Night Activities Schedule
 // Christina Thorpe
 // February 13, 2014
 
-// Assign variables and prompts
-var readingHours = prompt("Enter number of hours Christian reads on school nights:", 1);
-var gamingHours = prompt("Enter number of hours Christian games on school nights:", 1);
-var homewrkHours = prompt("Enter number of hours Christian does homework on school nights:", 1.5);
+// Assign variables and prompts for Christian's school night activities
+var homewrkHours = prompt("Enter number of hours Christian does homework each school night:", 1.5);
+var readingHours = prompt("Enter number of hours Christian reads each school night:", 1);
+var gamingHours = prompt("Enter number of hours Christian games each school night:", 1);
+
+// Calculate 4 week totals of Christian's school night activities
+var homewrkTotal = homewrkHours * 5 * 4;
+var readingTotal = readingHours * 5 * 4;
+var gamingTotal = gamingHours * 5 * 4;
