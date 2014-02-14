8f56a2ebc434dbaf5473d42f50d69bbe49c61b00
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-13 22:09:15 -0500

    Calculate hours worked M-F

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,3 +5,5 @@
 // Enter array of hours worked M-F
 var workHours = [5,4,5,4,5];
 
+// Calculate hours worked M-F
+var totalHours = workHours[0] + workHours[1] + workHours[2] + workHours[3] + workHours[4];
