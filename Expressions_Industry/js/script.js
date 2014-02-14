50a14d71ec17477626881ed377d5b1c382fc0b3c
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-13 23:08:39 -0500

    Create variable for the print console result

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,11 +1,12 @@
-// Expressions_Industry
+// Expressions_Industry - My Weekly Routine
 // Christina Thorpe
 // February 13, 2014
 
 // Enter prompt for preparing my son for school M-F
-var schoolPrep = prompt("Enter amount of hours to prepare my son for school M-F");
-// Parse integer for hours of shcool prep M-F
+var schoolPrep = prompt("Enter amount of hours to prepare my son for school M-F",1.5);
+// Parse integer for hours of school prep M-F
 var schoolprepInt = schoolPrep * 5;
+
 
 // Enter array of hours worked M-F
 var workHours = [5,4,5,4,5];
@@ -25,6 +26,8 @@ var studyHours = [5,5,5,5,5];
 // Calculate school hours M-F
 var totalStudy = studyHours[0] + studyHours[1] + studyHours[2] + studyHours[3] + studyHours [4];
 
+// Create variable for print console result
+var myweeklyRoutine = "Monday through Friday, my weekly routine is: \n- spending " + schoolprepInt + " morning hours preparing my son for school; \n- working a total of " + totalWork + " hours; " + "\n- taking " + lunchInt + " hours for lunchtime; and \n- making sure to study for at least " + totalStudy + " hours for school"
 
-// Print total work, lunch and school hours M-F
-console.log("Monday through Friday I work " + totalWork + " hours, " + "take " + lunchInt + " hours for lunch and make sure to study at least " + totalStudy + " hours for school.");
+// Print total school prep, work, lunch and school hours M-F
+console.log(myweeklyRoutine);
