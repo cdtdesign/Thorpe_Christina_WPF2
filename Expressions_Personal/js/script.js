656cbb320afd4c28b10fd0abb11c010429330615
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-13 20:52:38 -0500

    Comment homework variable

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,7 +2,7 @@
 // Christina Thorpe
 // February 13, 2014
 
-// Assign variables and prompts for Christian's school night activities
+// Assign variable and prompt for Christian's homework activity on school nights
 var homewrkHours = prompt("Enter number of hours Christian does homework each school night:", 1.5);
 var readingHours = prompt("Enter number of hours Christian reads each school night:", 1);
 var gamingHours = prompt("Enter number of hours Christian games each school night:", 1);
@@ -17,3 +17,6 @@ var homewrkString = "The amount of homework Christian completes in four weeks is
 var readingString = "The amount of reading Christian completes in four weeks is ";
 var gamingString = "The amount of gaming Christian plays in four weeks is ";
 
+var activitiesTotal = homewrkString + homewrkTotal + " hours. " +  readingString + readingTotal + " hours. " + gamingString + gamingTotal + " hours.";
+
+console.log(activitiesTotal);
