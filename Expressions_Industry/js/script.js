69f8ff94b56df272d001e52ceac31ae46539e0d9
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-13 22:12:11 -0500

    Enter array of lunch hours M-F

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,9 +4,11 @@
 
 // Enter array of hours worked M-F
 var workHours = [5,4,5,4,5];
-
 // Calculate hours worked M-F
 var totalWork = workHours[0] + workHours[1] + workHours[2] + workHours[3] + workHours[4];
+
+// Enter array of lunch hours M-F
+var lunchHours = [1,1,1,1,1];
 
 // Print total work hours M-F
 console.log(totalWork);
