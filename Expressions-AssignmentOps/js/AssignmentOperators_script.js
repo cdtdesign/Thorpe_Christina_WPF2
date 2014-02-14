bcb74e1ba6e6687466fdf9f4b9ac1581ef8e2a10
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-14 03:16:42 -0500

    Unrelated

---
+++ Expressions-AssignmentOps/js/AssignmentOperators_script.js
@@ -0,0 +1,30 @@
+//Expressions - Assignment Operators
+
+var a = 3;
+a += 4; //a = a + 4
+//console.log(a);
+
+
+var a = 3;
+a *= 4; //a = a * 4          
+//console.log(a);
+
+
+var a = 3;
+a /= 4; //a = a / 4
+//console.log(a);
+
+
+var a = 3;
+a -= 4; //a = a - 4
+//console.log(a);
+
+
+var a = 3;
+a ++; //a = a + 1 // a += 1
+//console.log(a);
+
+
+var a = 3;
+a --; //a = a - 1 // a -= 1
+console.log(a);
