43735619b6dd8017b60ac7a7002b48accb9b6663
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-17 16:08:51 -0500

    Logical Ops exercise

--- Conditionals/LogicalOperators/js/script.js
+++ Conditionals/LogicalOperators/js/script.js
@@ -1,17 +1,18 @@
 // Logical Operators
 
 // And && - Requires both in the pair to be TRUE for the whole pair to be TRUE.
-// Or || - Requires at leaast ONE of the pair to be TRUE got yhr eholr pair to bo TRUE.
+// Or || - Requires at least ONE of the pair to be TRUE for the whole pair to bo TRUE.
 // Exclusive or (XOR) ^
-// Not !
+// Not ! - Flips a value: TRUE becomes FALSE; FALSE becomes TRUE; is not used for comparison; negation
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 // If the price of the phone is less our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!!");
 }
+
