bb336ccedb5dab37e81a93909785cc3a003878bf
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-17 16:24:05 -0500

    Ternary Ops exercise

---
+++ Conditionals/TernaryOperators/js/script.js
@@ -0,0 +1,38 @@
+// Conditional Logic - Ternary Operators
+
+/*if(condition){
+    do if true;
+}else{
+    do if false;
+}
+
+(condition) ? do if true : do if false;
+*/
+
+/*var gpa = 48
+
+// If the GPA is over the min 2.0 score, the student can graduate
+/*if(gpa > 2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA is too low!");
+}
+*/
+
+/*(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
+*/
+
+var age = 11;
+var book;
+
+// If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+
+/*if(age < 10){
+    book = "Green Eggs and Ham";
+}else{
+    book = "The Time Machine";
+}
+
+console.log(book);
+*/
+
