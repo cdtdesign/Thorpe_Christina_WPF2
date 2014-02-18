1038fb07c497723742d32c06fdbbcb6d09092820
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 17:51:03 -0500

    Removed files from renamed folder

--- Conditionals/ConditionalsExample/js/script.js
+++
@@ -1,9 +0,0 @@
-// Conditionals Example
-
-var oldEnough = true;
-
-// If the child is old enough print to the console "you can ride!"
-if(oldEnough){
-    // Code performed if the condition is true
-    console.log("You can ride the coaster!");
-}
