6aec03110bd71401f4159ca18bfcfe741702fa02
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 15:52:06 -0500

    Re-order/edit conditions of login results

--- Conditionals_worksheet/Check the Login/js/script.js
+++ Conditionals_worksheet/Check the Login/js/script.js
@@ -3,27 +3,27 @@
 // February 17, 2014
 
 
-// Create alert statement that provides user with username and password
-var userLogin = alert("Username is: " + userName + "\nPassword is: " + userPass + "\nBoth are case-sensative.");
-
 // Correct username and password
 var userName = "FullSail";
 var userPass = "fullsail1979";
 var encryptName = "********";
 var encryptPass = "********";
 
+// Create alert statement that provides user with username and password
+var userLogin = alert("Username is: " + userName + "\nPassword is: " + userPass + "\nBoth are case-sensative.");
+
 // Prompt user for login
 var usernameEntry = prompt("Enter username: ", encryptName);
 var userpassEntry = prompt("Enter password: ", encryptPass);
 
 // Conditions for user login
-if(usernameEntry = userName && userpassEntry = userPass){
+if(usernameEntry != userName){
+// Result of incorrect username
+    console.log("User not found.");
+}else if(userpassEntry != userPass){
+// Result of incorrect password
+    console.log("Password does NOT match our records.");
+}else{
 // Result of correctly entered login
     console.log("Welcome, FullSail!");
-// Result of incorrect username
-}else if(!(usernameEntry = userName)){
-    console.log("User not found.");
-// Result of incorrect password
-}else if(!(userpassEntry = userPass)){
-    console.log("Password does NOT match our records.");
 }
