724e712de024e6988fbdff4ca6e520647bc986c5
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 00:37:47 -0500

    Edits to conditionals and variables

--- Conditionals_worksheet/Last Chance for Gas/js/script.js
+++ Conditionals_worksheet/Last Chance for Gas/js/script.js
@@ -4,18 +4,19 @@
 
 
 // List variables
-var mpg = 30
-var gallonGasCapacity = 15
+var mpg = 30;
+var gasCapacity = 15;
 
-var percentGas = Number(prompt("Percentage of gas left in tank? ")) + "%";
+// Prompt for user to input remaining percentage of gas
+var percentGas = Number(prompt("Percentage of gas left in tank?", 50)) * .01;
 
-var totalGas = mpg * gallonGasCapacity / (percentGas * .01);
+var totalGas = gasCapacity * percentGas;
 
-// List conditionals to find out if we can make it to the next gas station of 200 miles away
-if(totalGas > 45){
+// List conditional for having more than 46% tank full of gasoline
+if(percentGas > .46){
     console.log("Yes, you can make it without stopping for gas!");
 }
-
-if(totalGas <= 45){
+// List conditional for having 45% or less tank full of gasoline
+if(percentGas <= .45){
     console.log("You only have " + totalGas + " gallons of gas in your tank, better get gas now while you can!");
 }
