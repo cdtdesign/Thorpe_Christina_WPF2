35bc1caf1501393c24413a00fc65ac2ae4cc0a18
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 15:07:20 -0500

    Create alert advising user of username and password to enter.

--- Conditionals_worksheet/Check the Login/js/script.js
+++ Conditionals_worksheet/Check the Login/js/script.js
@@ -3,7 +3,17 @@
 // February 17, 2014
 
 
-// Correct username and password
-var userName = FullSail
-var userPass = fullsail1979
+// Create alert statement that provides user with username and password
+var userLogin = alert("Username is: FullSail" + "\nPassword is: fullsail1979" + "\nBoth are case-sensative.");
 
+// Correct username and password
+var userName = "FullSail";
+var userPass = "fullsail1979";
+var encryptName = "********";
+var encryptPass = "********";
+
+// Prompt user for login
+var usernameEntry = prompt("Enter username: ", encryptName);
+var userpassEntry = prompt("Enter password: ", encryptPass);
+
+console.log(usernameEntry + userpassEntry);
