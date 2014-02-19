0486c17567870d9929db0cc73c6d50ef46805405
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 20:25:22 -0500

    Distance to Christian's school

--- Conditional_assignment/Conditionals_Personal/js/script.js
+++ Conditional_assignment/Conditionals_Personal/js/script.js
@@ -1,5 +1,7 @@
-// Conditionals - Personal
+// Conditionals - Personal: Bike or Drive Christian to School?
 // Christina Thorpe
 // February 18, 2014
 
 
+// Distance from our house to Christian's school
+var schoolDistance = .8 + "miles";
