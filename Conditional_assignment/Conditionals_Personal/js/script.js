c8151146226fe030266eb617e80406dc0f635ea8
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 22:41:36 -0500

    Add prompt validation

--- Conditional_assignment/Conditionals_Personal/js/script.js
+++ Conditional_assignment/Conditionals_Personal/js/script.js
@@ -9,8 +9,18 @@ var schoolDistance = .8 * 4; // 3.2 miles per day
 var driveTime = 5 * 4; // 20 minutes per day
 var bikeTime = 15 * 4; // 60 minutes per day
 // Prompt for weather forecast of sunny vs. rainy school days---determining factor of biking vs. driving
-var sunnyDays = prompt("How many sunny days in this week's forecast? ");
-var rainyDays = Number(prompt("How many rainy days in this week's forecast? ", 5 - sunnyDays));
+var sunnyDays = prompt("How many sunny school days in this week's forecast?");
+
+// Variable for sunnyDays left blank
+var blanksunnyDays = "";
+
+// Prompt validation
+if(sunnyDays == blanksunnyDays){
+    alert("Whoahhh!! Please enter 0-5 sunny school days!")
+    sunnyDays = prompt("How many sunny school days in this week's forecast?");
+}
+
+var rainyDays = Number(prompt("How many rainy school days in this week's forecast?", 5 - sunnyDays));
 
 // Result variables
 var bikeDays = "If there are " + sunnyDays + " sunny days this week, a total of " + bikeTime * sunnyDays + " minutes of cardio will have been biked to school and " + schoolDistance * rainyDays + " miles will have been driven.";
