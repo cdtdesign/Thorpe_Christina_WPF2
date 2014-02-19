881b3588a0a86b772a6f059d6561e5bc0964bbc9
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 22:47:00 -0500

    Add prompt comment

--- Conditional_assignment/Conditionals_Personal/js/script.js
+++ Conditional_assignment/Conditionals_Personal/js/script.js
@@ -8,7 +8,7 @@ var schoolDistance = .8 * 4; // 3.2 miles per day
 // Travel time from hour house to Christian's school and back, driving and biking
 var driveTime = 5 * 4; // 20 minutes per day
 var bikeTime = 15 * 4; // 60 minutes per day
-// Prompt for weather forecast of sunny vs. rainy school days---determining factor of biking vs. driving
+// Prompt for weather forecast of sunny school days---determining factor of biking vs. driving
 var sunnyDays = prompt("How many sunny school days in this week's forecast?");
 
 // Variable for sunnyDays left blank
@@ -19,7 +19,7 @@ if(sunnyDays == blanksunnyDays){
     alert("Whoahhh!! Please enter 0-5 sunny school days!")
     sunnyDays = prompt("How many sunny school days in this week's forecast?");
 }
-
+//Prompt for weather forecast of rainy school days---determining factor of biking vs. driving
 var rainyDays = Number(prompt("How many rainy school days in this week's forecast?", 5 - sunnyDays));
 
 // Result variables
