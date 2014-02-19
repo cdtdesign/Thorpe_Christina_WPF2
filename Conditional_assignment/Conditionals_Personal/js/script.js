bdb5c5fffad1375d5b0744f231bdff212f6416b3
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 20:47:05 -0500

    User input of sunny vs. rainy days

--- Conditional_assignment/Conditionals_Personal/js/script.js
+++ Conditional_assignment/Conditionals_Personal/js/script.js
@@ -8,3 +8,6 @@ var schoolDistance = .8 * 2 + " miles";
 // Travel time from hour house to Christian's school and back, driving and biking
 var driveTime = 5 * 2 + " minutes";
 var bikeTime = 15 * 2 + " minutes";
+// Prompt for weather forecast of sunny vs. rainy school days---determining factor of biking vs. driving
+var sunnyDays = prompt("How many sunny days in this week's forecast? ", 3);
+var rainyDays = prompt("How many rainy days in this week's forecast? ");
