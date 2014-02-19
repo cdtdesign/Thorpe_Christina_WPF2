5f95cf8d26dec3971cd3d1d02c80f9e1a8ad352b
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 20:30:38 -0500

    Travel time from hour house to Christian's school

--- Conditional_assignment/Conditionals_Personal/js/script.js
+++ Conditional_assignment/Conditionals_Personal/js/script.js
@@ -3,5 +3,8 @@
 // February 18, 2014
 
 
-// Distance from our house to Christian's school
-var schoolDistance = .8 + "miles";
+// Distance from our house to Christian's school and back
+var schoolDistance = .8 * 2 + " miles";
+// Travel time from hour house to Christian's school and back, driving and biking
+var driveTime = 5 * 2 + " minutes";
+var bikeTime = 15 * 2 + " minutes";
