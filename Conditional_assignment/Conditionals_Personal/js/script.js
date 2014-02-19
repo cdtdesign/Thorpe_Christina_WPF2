3f936447ae0ba22d90499273bce7924f412819ab
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-18 21:03:50 -0500

    Adjust variables to only be numeric by removing string elements

--- Conditional_assignment/Conditionals_Personal/js/script.js
+++ Conditional_assignment/Conditionals_Personal/js/script.js
@@ -3,14 +3,14 @@
 // February 18, 2014
 
 
-// Distance from our house to Christian's school and back
-var schoolDistance = .8 * 2 + " miles";
+// Miles from our house to Christian's school and back
+var schoolDistance = .8 * 2;
 // Travel time from hour house to Christian's school and back, driving and biking
-var driveTime = 5 * 2 + " minutes";
-var bikeTime = 15 * 2 + " minutes";
+var driveTime = 5 * 2;
+var bikeTime = 15 * 2;
 // Prompt for weather forecast of sunny vs. rainy school days---determining factor of biking vs. driving
 var sunnyDays = prompt("How many sunny days in this week's forecast? ", 3);
-var rainyDays = prompt("How many rainy days in this week's forecast? ");
+var rainyDays = prompt("How many rainy days in this week's forecast? ", 2);
 // Minutes of cardio vs. mileage for the school week
 if(sunnyDays >= 3){
     console.log("A total of " + bikeTime * sunnyDays + " minutes of cardio will have been biked.");
