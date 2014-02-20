dd31b090829462d2afb6217237b57b059e9f9eb6
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-19 23:19:42 -0500

    Edits to graphic design validation prompt

--- Conditional_assignment/Conditionals_Industry/js/script.js
+++ Conditional_assignment/Conditionals_Industry/js/script.js
@@ -4,23 +4,22 @@
 
 
 // Variable of rates for various design projects
-var printDesignRate = 65; // Print design hourly rate
+var graphicDesignRate = 65; // Print design hourly rate
 var webDesignRate = 75; // Web design hourly rate
 var logoFlatRate = 500; // Logo design flat rate
 
 // Prompt asking how many hours of graphic design this month
-var graphicHours = Number(prompt("Enter print design hours this month (avg is 80-100 hrs"));
+var graphicHours = Number(prompt("Enter print design hours this month \n(avg is 80-100 hrs)"));
 
 // "else if" Validation prompt for graphic hours
 var graphicHrsAgain;
 if(graphicHours == "" || graphicHours <= 69){
-    graphicHrsAgain = Number(prompt("Wait! Check amount of hours! \nShould be at least:", 70 + " hours or more."))
-}else if(graphicHours >= 121){
-    graphicHrsAgain = Number(prompt("Wait! Check amount of hours! \nShould be no more than:", 120 + " hours or less."))
+    graphicHrsAgain = Number(prompt("Wait! Check amount of hours! \nShould be between:", 70 + "-" + 130))
+}else if(graphicHours >= 131){
+    graphicHrsAgain = Number(prompt("Wait! Check amount of hours! \nShould be no more than:", 130 + " hours or less."))
 }else{
-    graphicHrsAgain = Number(prompt("Enter print design hours this month (avg is 80-100 hrs"));
-}
 
+}
 // Prompt asking how many hours of web design this month
 var webHours = Number(prompt("Enter web design hours this month:"));
 // Prompt asking how many logo projects this month
