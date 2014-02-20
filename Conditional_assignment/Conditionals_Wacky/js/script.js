4cc42b1cf1bb803ac4d113f7f2cbc657f218e6f7
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-20 13:11:58 -0500

    Third little piggy variable prompt AND edits to previous variable prompts

--- Conditional_assignment/Conditionals_Wacky/js/script.js
+++ Conditional_assignment/Conditionals_Wacky/js/script.js
@@ -5,8 +5,11 @@
 
 // Living in the last standing house made of brick, one day the three little piggys went out for some items. How many items did they successfully bring back home?
 
-// Number of items piggy #1 bought
-var firstLittlePiggy = prompt("PIGGY #1: How many eggs did the fist little piggy buy from the market?");
+// Number of items piggy #1 got
+var firstLittlePiggy = parseInt(prompt("PIGGY #1: How many eggs did the fist little piggy buy from the market?"));
 
-// Number of items piggy #2 bought
-var secondLittlePiggy = prompt("Piggy #2: \nHow many lattes did the second little piggy buy from Starbucks?");
+// Number of items piggy #2 got
+var secondLittlePiggy = parseInt(prompt("PIGGY #2: \nHow many lattes did the second little piggy buy from Starbucks?"));
+
+// Number of items piggy #3 got
+var thirdLittlePiggy = parseInt(prompt("PIGGY #3: \nHow many flowers did the third little piggy get from the park?"));
