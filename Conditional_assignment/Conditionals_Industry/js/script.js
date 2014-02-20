7929c8838c67852ab1846fc9ff06907662dc0f14
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-19 23:32:42 -0500

    Added web design validation prompt here w/ comments

--- Conditional_assignment/Conditionals_Industry/js/script.js
+++ Conditional_assignment/Conditionals_Industry/js/script.js
@@ -9,19 +9,33 @@ var webDesignRate = 75; // Web design hourly rate
 var logoFlatRate = 500; // Logo design flat rate
 
 // Prompt asking how many hours of graphic design this month
-var graphicHours = Number(prompt("Enter print design hours this month \n(avg is 80-100 hrs)"));
+var graphicHours = Number(prompt("Enter print design hours this month: \n(avg is 80-100 hrs)"));
 
-// "else if" Validation prompt for graphic hours
+// "else if" Validation prompt for graphic design hours
 var graphicHrsAgain;
+// Logical & relational operators used here
 if(graphicHours == "" || graphicHours <= 69){
     graphicHrsAgain = Number(prompt("Wait! Check amount of hours! \nShould be between:", 70 + "-" + 130))
-}else if(graphicHours >= 131){
+}else if(graphicHours >= 131){ // Relational operator used here
     graphicHrsAgain = Number(prompt("Wait! Check amount of hours! \nShould be no more than:", 130 + " hours or less."))
 }else{
 
 }
+
 // Prompt asking how many hours of web design this month
-var webHours = Number(prompt("Enter web design hours this month:"));
+var webHours = Number(prompt("Enter web design hours this month: \n(avg is 60-80 hrs)"));
+
+// "else if" Validation prompt for web design hours
+var webHrsAgain;
+// Logical & relational operators used here
+if(webHours == "" || webHours <= 49){
+    webHrsAgain = Number(prompt("Wait! Check amount of hours! \nShould be between:", 50 + "-" + 90))
+}else if(webHours >= 91){ // Relational operator used here
+    webHrsAgain = Number(prompt("Wait! Check amount of hours! \nShould be no more than:", 90 + " hours or less."))
+}else{
+
+}
+
 // Prompt asking how many logo projects this month
 var logoProjects = Number(prompt("Enter logo projects this month:"));
 
