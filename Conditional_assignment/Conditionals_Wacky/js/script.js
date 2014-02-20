daeb6bd864fe0d083be10a10ca80fd7b9c7ab872
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-20 13:01:53 -0500

    First little piggy variable prompt

--- Conditional_assignment/Conditionals_Wacky/js/script.js
+++ Conditional_assignment/Conditionals_Wacky/js/script.js
@@ -1,5 +1,9 @@
-// Conditionals - Wacky
+// Conditionals - Wacky: Three Little Piggys
 // Christina Thorpe
 // February 18, 2014
 
 
+// Living in the last standing house made of brick, one day the three little piggys went out for some items. How many items did they successfully bring back home?
+
+// Number of items piggy #1 bought
+var firstLittlePiggy = prompt("PIGGY #1: How many eggs did fist little piggy buy from the market?");
