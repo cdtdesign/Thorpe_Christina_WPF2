9d88b5fe1fb610817bc5a7ef3424e9594ff0cde6
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-19 22:26:31 -0500

    Prompt 1

--- Conditional_assignment/Conditionals_Industry/js/script.js
+++ Conditional_assignment/Conditionals_Industry/js/script.js
@@ -8,3 +8,5 @@ var printDesignRate = 65; // Print design hourly rate
 var webDesignRate = 75; // Web design hourly rate
 var logoFlatRate = 500; // Logo design flat rate
 
+// Prompt asking hour many hours of graphic design this month
+var graphicHours = Number(prompt("Enter print design hours this month:"));
