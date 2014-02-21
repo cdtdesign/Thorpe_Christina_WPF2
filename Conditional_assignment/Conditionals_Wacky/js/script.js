e88c396aa2dee5a088ec63362331d9d7f1ba7ae5
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-20 20:57:47 -0500

    Edits to variables

--- Conditional_assignment/Conditionals_Wacky/js/script.js
+++ Conditional_assignment/Conditionals_Wacky/js/script.js
@@ -9,14 +9,14 @@
 var firstPiggysStuff = Number(prompt("PIGGY #1 GOES TO THE MARKET: How many of the 6 eggs bought did the big bad wolf take?"));
 
 // First little piggy result variable
-var firstLittlePiggy = "The first little piggy lost " + firstPiggysStuff + " egg(s) to the big bad wolf.";
+var firstLittlePiggy = 'On the way home from the market, the first little piggy lost ' + firstPiggysStuff + ' egg(s) \nto the big bad wolf, and cried "whaaaaahhhh!" all the way home.';
 
 // Big Bad Wolf stops piggy #1 to take some of his stuff
 if(firstPiggysStuff == ""){
 var firstPiggyNada = Number(prompt("Come'on... everyone knows the wolf ALWAYS takes something. Please enter how many of the 6 eggs were taken."));
 
 // Big bad wolf result variable
-var bigBadWolfA = "The first little piggy lost " + firstPiggyNada + " egg(s) to the big bad wolf.";
+var bigBadWolfA = 'On the way home from the market, the first little piggy lost ' + firstPiggyNada + ' egg(s) \nto the big bad wolf, and cried "whaaaaahhhh!" all the way home.';
 
     console.log(bigBadWolfA);
 }
@@ -34,14 +34,14 @@ else{
 var secondPiggysStuff = parseInt(prompt("PIGGY #2 GOES TO STARBUCKS: How many of the 3 lattes did the big bad wolf take?"));
 
 // Second little piggy result variable
-var secondLittlePiggy = "The second little piggy lost " + secondPiggysStuff + " latte(s) to the big bad wolf.";
+var secondLittlePiggy = '\nOn the way home from Starbucks, the second little piggy lost ' + secondPiggysStuff + ' latte(s) \nto the big bad wolf, and cried "whaaaaahhhh!" all the way home.';
 
 // Big Bad Wolf stops piggy #2 to take some of his stuff
 if(secondPiggysStuff != ""){
 var secondPiggyNada = Number(prompt("Come'on... everyone knows the wolf ALWAYS takes something. Please enter how many lattes were taken."));
 
 // Big bad wolf result variable
-var bigBadWolfB = "The second little piggy lost " + secondPiggyNada + " latte(s) to the big bad wolf.";
+var bigBadWolfB = '\nOn the way home from Starbucks, the second little piggy lost ' + secondPiggyNada + ' latte(s) \nto the big bad wolf, and cried "whaaaaahhhh!" all the way home.';
 
     console.log(bigBadWolfB);
 }
@@ -59,19 +59,5 @@ else{
 var thirdPiggysStuff = parseInt(prompt("PIGGY #3 GOES TO THE PARK: How many of the 6 flowers picked did the big bad wolf take?"));
 
 // Third little piggy result variable
-var thirdLittlePiggy = "The third little piggy lost " + thirdPiggysStuff + " latte(s) to the big bad wolf.";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var thirdLittlePiggy = '\nOn the way home from the park, the third little piggy lost ' + thirdPiggysStuff + ' flower(s) \nto the big bad wolf, and cried "whaaaaahhhh!" all the way home.';
 
