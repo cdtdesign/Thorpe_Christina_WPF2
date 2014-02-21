003852766fbd1200e26ce1f6c24cadb7fa922bf7
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-20 19:50:48 -0500

    Second piggy result variable

--- Conditional_assignment/Conditionals_Wacky/js/script.js
+++ Conditional_assignment/Conditionals_Wacky/js/script.js
@@ -1,29 +1,32 @@
-// Conditionals - Wacky: Three Little Piggys
+// Conditionals - Wacky: Three Little Piggys on the Move!
 // Christina Thorpe
 // February 18, 2014
 
 
 // Living in the last standing house made of brick, one day the three little piggys went out for some items. How many items did they successfully bring back home?
 
-// Number of items piggy #1 got
-var firstPiggysStuff = parseInt(prompt("PIGGY #1: How many of the 6 eggs did the big bad wolf take from the first little piggy on his way home from the market?"));
-
-// Number of items piggy #2 got
-var secondPiggysStuff = parseInt(prompt("PIGGY #2: \nHow many of the 3 lattes did the big bad wolf take from the second little piggy on his way home from Starbucks?"));
-
-// Number of items piggy #3 got
-var thirdPiggysStuff = parseInt(prompt("PIGGY #3: \nHow many of the 6 flowers did the big bad wolf take from the third little piggy pick on his way home from the park?"));
-
-var bigBadWolf;
-
-// Big Bad Wolf stops piggys #1 and #3 to take some of their items
-if(firstPiggysStuff == "" || firstPiggysStuff >= 7){
-    firstPiggysStuff = parseInt(prompt("PIGGY #1: How many of the 6 eggs did the big bad wolf take from the first little piggy on his way home from the market?"));
+// Number of items piggy #1 had taken by the wolf
+var firstPiggysStuff = Number(prompt("PIGGY #1 GOES TO THE MARKET: How many of the 6 eggs bought did the big bad wolf take?"));
 
 // First little piggy result variable
-var firstLittlePiggy = "The first little piggy lost " + firstPiggysStuff + " eggs to the big bad wolf.";
+var firstLittlePiggy = "The first little piggy lost " + firstPiggysStuff + " egg(s) to the big bad wolf.";
+
+// Big Bad Wolf stops piggy #1 to take some of his stuff
+if(firstPiggysStuff == ""){
+    Number(prompt("Come'on... everyone knows the wolf ALWAYS takes something. Please enter how many of the 6 eggs were taken."))}
 
 // Print result of first little piggy
-}else if(firstPiggysStuff <= 6 - 1){
+else if(firstPiggysStuff <= 6 - 1)
+{
     console.log(firstLittlePiggy);
 }
+else{
+
+}
+
+// Number of items piggy #2 had taken by the wolf
+var secondPiggysStuff = parseInt(prompt("PIGGY #2 GOES TO STARBUCKS: How many of the 3 lattes did the big bad wolf take?"));
+
+// Second little piggy result variable
+var secondLittlePiggy = "The second little piggy lost " + secondPiggysStuff + " latte(s) to the big bad wolf.";
+
