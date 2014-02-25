2abe401abebc236d5da38f1d5e310825f26e68aa
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-24 22:38:15 -0500

    Function invocation

---
+++ Functions SCREENCASTS/2_FunctionInvocation/js/script.js
@@ -0,0 +1,19 @@
+//Function Invocation
+
+
+// Function definition
+function outputMsg(){
+	console.log("Hello World!");
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
+
+// Function invocation OR "function call" OR "calling the function"
+calcArea();
+calcArea();
+calcArea();
