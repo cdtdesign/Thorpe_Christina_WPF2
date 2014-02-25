7a89d8badce444b49b8397e5eb9be49df1f66a1b
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-24 22:25:57 -0500

    Basic Function Structure

---
+++ Functions SCREENCASTS/1_BasicFunctionStructure/js/script.js
@@ -0,0 +1,17 @@
+// Basic Function Structure
+
+/*function functionName(){
+	//code the function runs
+}
+*/
+
+function outputMsg(){
+	console.log("Hello World!");
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
