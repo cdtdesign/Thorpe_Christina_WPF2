2608131f2883519444284150bcdc9eec580e5060
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-25 13:46:12 -0500

    Function container with parameters

--- Functions_worksheet/Functions_Circumference/js/script.js
+++ Functions_worksheet/Functions_Circumference/js/script.js
@@ -3,3 +3,11 @@
 // February 25, 2014
 
 
+// Calculate the circumference of a circle
+
+// Function container with parameters
+function circleCircum(inches){ // Parameter: C = 2∏r
+    var circleCircum = inches * 3.14 * rad
+}
+
+
