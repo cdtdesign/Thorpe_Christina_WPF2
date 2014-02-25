2d9a28c1722ec362d9762ca0b12abba86b9bc297
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-24 23:10:44 -0500

    Returning Values

---
+++ Functions SCREENCASTS/5_ReturningValules/js/script.js
@@ -0,0 +1,11 @@
+// Retunring Values
+
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h){ // w=30, h=20
+	var area = w * h;
+	return area; // funciton spitting the info out
+}
+
+console.log(total);
