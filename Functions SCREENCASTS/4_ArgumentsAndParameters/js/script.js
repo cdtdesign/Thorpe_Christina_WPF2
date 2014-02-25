c5f332f4ef06de99de16d61cdb45fb5e3795e6dd
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-24 23:00:35 -0500

    Arguments and Parameters

---
+++ Functions SCREENCASTS/4_ArgumentsAndParameters/js/script.js
@@ -0,0 +1,28 @@
+// Arguments and Parameters
+
+
+// function call
+/*functionName(argument1, argument2);
+function functionName(parameter1, parameter2){
+	//code the function runs
+}
+*/
+
+/*calcArea(30, 20);
+
+function calcArea(w, h){ // w=30, h=20
+	var area = w * h;
+	console.log(area);
+}
+*/
+
+function dogYears(age){ // parameters
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + " years old.")
+}
+var age1 = 6;
+dogYears(age1); // arguments can be set as variables
+
+// OR
+
+dogYears(4); // arguments can be numeric
