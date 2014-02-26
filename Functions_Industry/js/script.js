ed46727b404bb3c89f484bdcdfe178154d08484c
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-26 11:11:59 -0500

    Data service varaiables

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,4 +1,9 @@
-// Functions - Industry
+// Functions - Industry: Monthly Business Data Services
 // Christina Thorpe
 // February 26, 2014
+
+// Monthly business data service cost variables
+var dataStorage = 10; // Currently DropBox
+var projectInvoicing = 12; // Currently Harvest
+var projectManager = 20; // Currently Basecamp
 
