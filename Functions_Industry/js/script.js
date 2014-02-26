4abf9ed77cf51c11f2cf28b8e442dc5b3717add7
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-26 17:15:19 -0500

    Add calculating statement

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,6 +2,8 @@
 // Christina Thorpe
 // February 26, 2014
 
+// Calculate business data services cost for a given number of months
+
 // Monthly business data services cost variables
 var dataStorageCost = 10; // Current data storage is DropBox
 var projectInvCost = 12; // Current invoicing is thru Harvest
