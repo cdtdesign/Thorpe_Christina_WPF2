9d5ff6cdc18dc9239a7149c2fb0b8055fe1ff9c1
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-27 18:41:20 -0500

    Alert containing a simple math puzzle

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,4 +2,5 @@
 // Christina Thorpe
 // February 26, 2014
 
-
+// Here's a puzzle, to make your puzzler sore:
+var puzzledRapunzel = alert("HOW MANY BRICKS HIGH, RAPUNZEL?\xBF?\xBF?\xBF?\xBF?\xBF?\xBF? \rV   -------------------------------------\r))   Guess how many tower bricks tall is equal to \r((   Rapunzel's 70 ft mane. If each brick is 12 in \r))   wide and their height is 2/3 of their width. \r((   How many tower bricks tall would equal the \r))   length of her beautiful golden locks of hair?   \ro\rS");
