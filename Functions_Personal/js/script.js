30faf9c8d6de1b240b82c95c30cb0cd770f1329f
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-27 00:59:24 -0500

    Prompt variable for sleep time

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,7 +5,11 @@
 
 // Calculate number of hours slept
 
+// Prompt variables for sleep time
+var sleepStart = prompt("Time fell asleep? ", 11);
+var sleepAMPM = prompt("a = AM | p = PM");
+
 // Create normal named function container
 function hoursSlept(){
-    
+
 }
