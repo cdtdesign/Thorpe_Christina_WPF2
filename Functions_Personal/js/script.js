76f2d2c08b385aa226396a0cb47a5117c11f5571
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-27 15:32:48 -0500

    Comment edit on conditional for AM sleep start

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -12,7 +12,7 @@ var sleepAMPM1 = prompt("AM or PM?", "PM");
 var sleepEnd = prompt("Time woken up?", 6);
 var sleepAMPM2 = prompt("AM or PM?", "AM");
 
-// Create conditional for AM selection
+// Create conditional for AM selection on Sleep Start
 if(sleepAMPM1 === "AM" || sleepAMPM1 === "am"){  // Logical operator included
     var timeAsleep = (parseInt(sleepStart) + 12);
 }
