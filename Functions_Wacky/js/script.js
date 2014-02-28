44abfaab65e03f89a5978c494af94fc236b9d484
Christina Thorpe <cdtdesign@fullsail.edu>
2014-02-27 19:09:36 -0500

    Add in some constant variables

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,4 +3,11 @@
 // February 26, 2014
 
 // Here's a puzzle, to make your puzzler sore:
-var puzzledRapunzel = alert("HOW MANY BRICKS HIGH, RAPUNZEL?\xBF?\xBF?\xBF?\xBF?\xBF?\xBF? \rV   -------------------------------------\r))   Guess how many tower bricks tall is equal to \r((   Rapunzel's 70 ft mane. If each brick is 12 in \r))   wide and their height is 2/3 of their width. \r((   How many tower bricks tall would equal the \r))   length of her beautiful golden locks of hair?   \ro\rS");
+var puzzledRapunzel = alert("HOW MANY BRICKS HIGH, RAPUNZEL?\xBF?\xBF?\xBF?\xBF?\xBF?\xBF? \rV   -------------------------------------\r))   Guess how many tower bricks tall is equal to \r((   Rapunzel's 70 ft mane. If each brick is 12 in \r))   wide and their height is 2/3 of their width. \r((   How many tower bricks tall would equal the \r))   length of her beautiful golden locks of hair?   \ro\rS   Come'on... take a guess!!!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1!\xA1");
+
+// Create constant variables
+var RapunzelsHair = 70;
+var inchesPerFoot = 12;
+var brickLength = 12;
+var brickHeight = .66666667;
+
